d6aa6220e11f286f4b79aff74d43d60a0673010f
Prince <princejhabak11.pj@gmail.com>
2020-03-14 12:51:41 +0530

    removed credentials

---
+++ README.md
@@ -0,0 +1,32 @@
+# Create File Sharing Web App with ReactJs, Nodejs,Mongob from Scratch.
+
+### Installation
+
+```
+git clone https://github.com/Princejhabak/file-sharing-app-react.js.git
+
+```
+
+```
+cd fileapp/api
+
+```
+``` 
+npm install 
+```
+
+### Development
+
+``` npm run dev ```
+
+
+## Start React App
+
+``` 
+cd app 
+
+```
+
+```
+npm start
+```
---
+++ api/src/config.js
@@ -0,0 +1,24 @@
+import {accessKeyId,secretAccessKey} from './s3-config.json'
+export const smtp = {
+    host: 'smtp.sendgrid.net',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: '', // generated ethereal user
+        pass: ''  
+    }
+};
+
+
+
+export const url = 'http://localhost:3001';
+
+export const s3Config = {
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+};
+
+
+export const s3Region = ''
+export const s3Bucket = ''
+
